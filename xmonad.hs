cae037a8f1a831e80065e434c8717d7487719be0
Jon Distad <jon.distad@gmail.com>
2019-06-09 16:03:55 -0400

    Play with key sequences

--- xmonad.hs
+++ xmonad.hs
@@ -1,3 +1,16 @@
 import XMonad
+import XMonad.Actions.ShowText (ShowTextConfig(..), flashText, handleTimerEvent)
+import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.Timer (startTimer)
 
-main = xmonad def { modMask = mod4Mask }
+main = xmonad (def { terminal = "urxvt"
+                   , modMask = mod4Mask
+                   , handleEventHook = handleTimerEvent
+                   }
+                `additionalKeysP` myKeys)
+  where
+    myKeys = [("M-x x", flashText stc 1 "M-x x")]
+    stc = def { st_font = "xft:FantasqueSansMono:size=18"
+              , st_bg   = "black"
+              , st_fg   = "white"
+              }
